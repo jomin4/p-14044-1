a50a8c9b4fab4f03a84c8312342253ddaa4f5bc4
jomin4 <jominseognam4@gmail.com>
2026-08-07 10:32:34 +0900

    005 : 9강 vpc-2 추가, 10강 variables.tf로 공통변수 분리

---
+++ infra/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  // aws 라이브러리 불러옴
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# AWS 설정 시작
+provider "aws" {
+  region = var.region
+}
+# AWS 설정 끝
+
+# VPC 설정 시작
+resource "aws_vpc" "vpc_1" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.prefix}-vpc-1"
+  }
+}
+
+resource "aws_vpc" "vpc_2" {
+  cidr_block = "10.1.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.prefix}-vpc-2"
+  }
+}
+# VPC 설정 끝
